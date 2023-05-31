e6f62a83521d347d3bc035e16d390233d3565a20
PolyakovDmitry546 <polyakov1803@gmail.com>
2023-05-31 14:18:11 +0500

    change message styles

--- messenger/static/js/components/message.js
+++ messenger/static/js/components/message.js
@@ -1,15 +1,19 @@
 export function getMessageNode(message, channel_type='group', is_author_of_message) {
     var messageDiv = document.createElement('div');
+    const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" };
+    const messageDate =  new Date(message.update_at).toLocaleTimeString([], dateOptions);
     messageDiv.innerHTML = 
-        `<div class="card ${is_author_of_message?"float-left":"float-right"} d-flex" style="max-width: 75%;">
-            <div class="card-body">
-                <h6 class="card-subtitle mb-2 text-body-secondary">
-                    <a href="">${message.author_username}</a>
-                </h6>
-                <p class="card-text">${message.text}</p> 
-                <p class="card-text text-end">
-                    <small class="text-body-secondary ">${new Date(message.update_at).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</small>
-                </p>
+        `<div class="d-flex ${is_author_of_message?"justify-content-start":"justify-content-end"}">
+            <div class="card" style="max-width: 70%; min-width: 30%;">
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-body-secondary">
+                        <a href="">${message.author_username}</a>
+                    </h6>
+                    <p class="card-text">${message.text}</p> 
+                    <p class="card-text text-end">
+                        <small class="text-body-secondary ">${messageDate}</small>
+                    </p>
+                </div>
             </div>
         </div>`;
 
