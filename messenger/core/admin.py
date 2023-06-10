8ab6853519484643b58e0196607181c12ffd597e
PolyakovDmitry546 <polyakov1803@gmail.com>
2023-06-10 12:31:07 +0500

    added base admin functionality

--- messenger/core/admin.py
+++ messenger/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
-# Register your models here.
+from . import models
+
+
+class ChannelMembersInLine(admin.StackedInline):
+    model = models.Membership
+
+
+class ChannelMessageInLine(admin.StackedInline):
+    model = models.Message
+
+@register(models.Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    inlines = [ChannelMembersInLine, ChannelMessageInLine]
+
+
+admin.site.register(models.Membership)
