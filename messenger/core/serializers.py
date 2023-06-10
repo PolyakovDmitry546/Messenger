48fb3eec8463d3540a9622f39f94372fcf9fa514
PolyakovDmitry546 <polyakov1803@gmail.com>
2023-06-10 12:27:56 +0500

    added serializers for receiving messages

---
+++ messenger/core/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Message
+
+
+class MessageAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    author = MessageAuthorSerializer()
+
+    class Meta:
+        model = Message
+        fields = ('text', 'update_at', 'author')
+
+
+class MessagePageSerializer(serializers.Serializer):
+    messages = MessageSerializer(many=True)
+    page = serializers.IntegerField()
+    next_page = serializers.IntegerField()
