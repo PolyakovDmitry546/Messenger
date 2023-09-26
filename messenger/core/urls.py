9938890b47f2b701ab53b0d7d4edb15a4b4ea011
PolyakovDmitry546 <polyakov1803@gmail.com>
2023-09-26 13:37:06 +0500

    Separate channel and dialog views, urls

--- messenger/core/urls.py
+++ messenger/core/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('channel/create', views.ChannelCreateView.as_view(), name='channel_create'),
     path('channels/<int:pk>', views.ChannelView.as_view(), name='channel'),
     path('channels/<int:pk>/join', views.ChannelJoinView.as_view(), name='channel_join'),
+    path('dialog/<int:pk>', views.DialogView.as_view(), name='dialog'),
+    path('dialog/<int:user_pk>/join', views.DialogJoinView.as_view(), name='dialog_join'),
     path('api/v1/channels/<int:pk>/messages', views.ChannelMessagesAPIView.as_view(), name='messages'),
+    path('api/v1/dialog/<int:pk>/messages', views.DialogMessagesAPIView.as_view(), name='dialog_messages'),
     path('api/v1/search', views.SearchResultAPIView.as_view(), name='search'),
 ]
--- messenger/core/views.py
+++ messenger/core/views.py
@@ -11,9 +11,9 @@ from rest_framework import permissions
 from rest_framework.views import APIView, Response
 
 from .forms import ChannelCreationForm
-from .models import Channel
-from .serializers import MessagePageSerializer, SearchResultSerializer
-from .services import ChannelMessagesPageService
+from .models import Channel, Dialog, DialogMembership
+from .serializers import ChannelMessagePageSerializer, DialogMessagePageSerializer, SearchResultSerializer
+from .services import ChannelMessagesPageService, DialogMessagesPageService
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
@@ -85,6 +85,43 @@ class ChannelJoinView(LoginRequiredMixin, View):
         return redirect('channel', pk=pk)
 
 
+class DialogView(LoginRequiredMixin, TemplateView):
+    template_name = 'dialog.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        user = self.request.user
+        dialog = Dialog.objects.get(pk=pk)
+        context = {
+            'context': {
+                'dialog': dialog,
+                'target_user': DialogMembership.objects.filter(chat=dialog).exclude(user=user).first().user,
+                'user_channels': user.channel_set.all(),
+                'messages_url': reverse('dialog_messages', kwargs={'pk': pk}),
+            },
+        }
+        return context
+
+
+class DialogJoinView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        template_name = 'dialog_join.html'
+        target_user_pk = kwargs.get('pk')
+        target_user = User.objects.get(pk=target_user_pk)
+        user = request.user
+
+        if (dialog := Dialog.get_dialog(user1=user, user2=target_user)) is not None:
+            return redirect('dialog', pk=dialog.pk)
+
+        context = {
+            'context': {
+                'dialog': None,
+                'user_channels': user.channel_set.all(),
+            },
+        }
+        return render(request, template_name, context)
+
+
 class ChannelMessagesAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -98,7 +135,24 @@ class ChannelMessagesAPIView(APIView):
         channel_service = ChannelMessagesPageService(request.user, channel_pk, page, per_page)
         obj = dict(messages=channel_service.get_messages(),
                    page=channel_service.get_page(), next_page=channel_service.get_next_page())
-        serializer = MessagePageSerializer(obj)
+        serializer = ChannelMessagePageSerializer(obj)
+        return Response(serializer.data)
+
+
+class DialogMessagesAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, **kwargs):
+        page = request.GET.get('page', default=None)
+        page = int(page) if page is not None else None
+        per_page = request.GET.get('per_page', default=None)
+        per_page = int(per_page) if per_page is not None else None
+        dialog_pk = kwargs.get('pk')
+
+        dialog_service = DialogMessagesPageService(request.user, dialog_pk, page, per_page)
+        obj = dict(messages=dialog_service.get_messages(),
+                   page=dialog_service.get_page(), next_page=dialog_service.get_next_page())
+        serializer = DialogMessagePageSerializer(obj)
         return Response(serializer.data)
 
 
