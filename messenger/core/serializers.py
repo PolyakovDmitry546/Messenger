79c5f2053a5869e106b10548cb442e33de50d9c9
PolyakovDmitry546 <polyakov1803@gmail.com>
2023-09-26 13:35:23 +0500

    Separate channel and dialog serializers

--- messenger/core/serializers.py
+++ messenger/core/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
-from .models import Channel, Message
+from .models import Channel, ChannelMessage, DialogMessage
 
 
 class MessageAuthorSerializer(serializers.ModelSerializer):
@@ -11,24 +11,40 @@ class MessageAuthorSerializer(serializers.ModelSerializer):
         fields = ('id', 'username')
 
 
-class MessageSerializer(serializers.ModelSerializer):
+class ChannelMessageSerializer(serializers.ModelSerializer):
     author = MessageAuthorSerializer()
 
     class Meta:
-        model = Message
+        model = ChannelMessage
         fields = ('text', 'update_at', 'author')
 
 
-class MessagePageSerializer(serializers.Serializer):
-    messages = MessageSerializer(many=True)
+class ChannelMessagePageSerializer(serializers.Serializer):
+    messages = ChannelMessageSerializer(many=True)
+    page = serializers.IntegerField()
+    next_page = serializers.IntegerField()
+
+
+class DialogMessageSerializer(serializers.ModelSerializer):
+    author = MessageAuthorSerializer()
+
+    class Meta:
+        model = DialogMessage
+        fields = ('text', 'update_at', 'author')
+
+
+class DialogMessagePageSerializer(serializers.Serializer):
+    messages = ChannelMessageSerializer(many=True)
     page = serializers.IntegerField()
     next_page = serializers.IntegerField()
 
 
 class SearchResultUserSerializer(serializers.ModelSerializer):
+    url = serializers.CharField(source='', read_only=True)
+
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username', 'url')
 
 
 class SearchResultChannelSerializer(serializers.ModelSerializer):
