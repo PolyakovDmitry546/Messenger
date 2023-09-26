42770699044a661285edda3a75a920922e4474bf
PolyakovDmitry546 <polyakov1803@gmail.com>
2023-09-26 13:34:44 +0500

    Separate channel and dialog consumers

--- messenger/core/consumers.py
+++ messenger/core/consumers.py
@@ -1,37 +1,52 @@
-# chat/consumers.py
 import json
+from abc import ABC, abstractmethod
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-from .models import Channel, Message
-from .serializers import MessageSerializer
+from .models import Channel, ChannelMessage, Dialog, DialogMessage, Message
+from .serializers import ChannelMessageSerializer, DialogMessageSerializer
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(ABC, AsyncWebsocketConsumer):
+    @abstractmethod
+    def make_room_group_name(self):
+        self.room_group_name = None
+
+    @abstractmethod
+    async def make_message_owner(self):
+        self.message_owner = None
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.channel = await Channel.objects.aget(pk=self.room_name)
+        self.make_room_group_name()
+        await self.make_message_owner()
         self.user = self.scope["user"]
-        self.room_group_name = "chat_%s" % self.room_name
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
+    @abstractmethod
+    def create_message(self, message_text) -> Message:
+        pass
+
+    @abstractmethod
+    def serialize_message(self, message):
+        pass
+
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        text_message = text_data_json["message"]["text"]
+        message_text = text_data_json["message"]["text"]
 
-        message = Message(author=self.user, text=text_message, channel=self.channel)
+        message = self.create_message(message_text)
         await message.asave()
 
-        serializer = MessageSerializer(message)
+        serializer = self.serialize_message(message)
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -44,3 +59,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps(message))
+
+
+class ChannelConsumer(ChatConsumer):
+    def make_room_group_name(self):
+        self.room_group_name = "channel_%s" % self.room_name
+
+    async def make_message_owner(self):
+        self.message_owner = await Channel.objects.aget(pk=self.room_name)
+
+    def create_message(self, message_text) -> Message:
+        return ChannelMessage(author=self.user, text=message_text, owner=self.message_owner)
+
+    def serialize_message(self, message):
+        return ChannelMessageSerializer(message)
+
+
+class DialogConsumer(ChatConsumer):
+    def make_room_group_name(self):
+        self.room_group_name = "dialog_%s" % self.room_name
+
+    async def make_message_owner(self):
+        self.message_owner = await Dialog.objects.aget(pk=self.room_name)
+
+    def create_message(self, message_text) -> Message:
+        return DialogMessage(author=self.user, text=message_text, owner=self.message_owner)
+
+    def serialize_message(self, message):
+        return DialogMessageSerializer(message)
